4a11b57a771a5664f12e1727c604e6a8f8df045d
Nirmesh Rastogi <nirmesh44@gmail.com>
2013-08-04 20:57:50 +0530

    enjoy my blog app i am loving it

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+class PostsController < ApplicationController
+def new
+    @post = Post.new
+end
+
+def index
+  @posts = Post.all
+end
+
+def create
+# render text: params[:post].inspect
+ @post = Post.new(params[:post])
+  if @post.save
+    redirect_to @post
+   else
+    render 'new'
+   end
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'dba817cffc7a8e2210db6c43a9bb8e7a0de3cfda0739ca010d90d19c00a3fd263a91d280792f7cd8804d766c2eb747a33f7c631f1c6aae3cf3a9b505c0367160'
