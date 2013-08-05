31ceefb6ca0baec16edf876562b4e36a7cb04bfa
nirmesh <nirmesh44@gmail.com>
2013-08-05 15:36:31 +0530

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class Post < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
   has_many :comments
   attr_accessible :text, :title
   validates :title,presence: true,length: { minimum: 5 }
